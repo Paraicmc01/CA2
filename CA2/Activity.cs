a019302c1abbb88bafad66c1e01235bca4b79949
Paraic Mc Donagh <S00188844@mail.itsligo.ie>
2019-11-26 16:40:53 +0000

    Ca2 Start

--- CA2/Activity.cs
+++ CA2/Activity.cs
@@ -6,16 +6,33 @@ using System.Threading.Tasks;
 
 namespace CA2
 {
+    public enum ActivityType { Air, Water, Land}
     public class Activity
     {
         //properties
+        public int ActivityDate { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+        public ActivityType TypeOfActivity { get; set; }
 
 
         //constructors
+        public Activity(int activitydate, string name, decimal cost, string description, ActivityType activitytype)
+        {
 
+            ActivityDate = activitydate;
+            Name = name;
+            Cost = cost;
+            Description = description;
+            TypeOfActivity = ActivityType;
+        }
 
         // methods
-
+        public override string ToString()
+        {
+            return base.ToString();
+        }
 
     }
 }
